bfebfc3aea9bac60f45398a490be2d05dacf7ce2
MRgrav <gauravnetuser@gmail.com>
2024-06-28 23:36:38 +0530

    alpha version done

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ ini_set('display_errors', 1);
 // Define constants for paths
 define('ROOT', __DIR__);
 
-define('FAVICON', __DIR__.'/public/favicon.ico');
-
 // config
 require_once 'config/config.php';
 // core files
@@ -18,62 +16,6 @@ require_once '.core/core.php';
 require_once 'routes/routes.php';
 // main header
 require_once 'public/index.php';
-
-
-
-// Attempt to connect to the database
-// $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// } else {
-//     echo "done";
-// }
-
-// $host = "localhost";
-// $db = "postgres";
-// $user = "postgres";
-// $password = "postgres";
-// $port = "5432";
-
-// $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=UTF8";
-
-// try {
-// 	$pdo = new PDO($dsn, $user, $password);
-
-// 	if ($pdo) {
-// 		echo "Connected to the $db database successfully!";
-// 	}
-// } catch (PDOException $e) {
-// 	echo $e->getMessage();
-// }
-
-
-
-
-// $sql = 'SELECT scheme_code FROM schemes';
-
-// $statement = $pdo->query($sql);
-
-// // get all publishers
-// $publishers = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// // echo $publishers;
-
-// if ($publishers) {
-// 	// show the publishers
-// 	foreach ($publishers as $publisher) {
-// 		echo $publisher['scheme_code'] . '<br>';
-// 	}
-// }
-
-
-
-
-
-
-
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
